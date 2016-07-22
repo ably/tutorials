c971a708cd02004bc4e7582c5095d828e82191f0
Matthew O'Riordan <matthew.oriordan@gmail.com>
2016-07-22 02:27:15 +0100

    Step 2 - Install Ably

---
+++ example.js
@@ -0,0 +1,7 @@
+const Ably = require('ably');
+
+const ApiKey = 'INSERT-YOUR-API-KEY-HERE'; /* Add your API key here */
+if (ApiKey.indexOf('INSERT') === 0) { throw('Cannot run without an API key. Add your key to example.js'); }
+
+/* Instance the Ably library */
+var rest = new Ably.Rest({ key: ApiKey });
