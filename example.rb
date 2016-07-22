75becbfd62d713e4e6e46067cc928f760b955be0
Matthew O'Riordan <matthew.oriordan@gmail.com>
2016-07-22 02:31:54 +0100

    Step 3 - Subscribe to messages

--- example.rb
+++ example.rb
@@ -6,4 +6,10 @@ raise "Cannot run without an API key. Add your key to example.rb" if API_KEY.inc
 EventMachine.run do
   # Instance the Ably library
   ably = Ably::Realtime.new(key: API_KEY)
+
+  # Subscribe to messages on the sport channel
+  channel = ably.channels.get('sport')
+  channel.subscribe do |msg|
+    puts "Received: #{msg.data}"
+  end
 end
