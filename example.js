f831895dd4883f5c48432d4ff4f6aba0b0e3c993
Matthew O'Riordan <matthew.oriordan@gmail.com>
2016-09-02 17:17:19 +0100

    Step 4 – Publishing a message

--- example.js
+++ example.js
@@ -3,6 +3,9 @@ const Ably = require("ably");
 const ApiKey = "INSERT-YOUR-API-KEY-HERE"; /* Add your API key here */
 if (ApiKey.indexOf("INSERT") === 0) { throw("Cannot run without an API key. Add your key to example.js"); }
 
+console.log("Run this script in multiple consoles concurrently.");
+console.log("Each script will simultaneously receive the published message as all clients are subscribed to the 'sport' channel.");
+
 /* Instance the Ably library */
 var realtime = new Ably.Realtime({ key: ApiKey });
 
@@ -10,4 +13,14 @@ var realtime = new Ably.Realtime({ key: ApiKey });
 var channel = realtime.channels.get("sport");
 channel.subscribe(function(msg) {
   console.log("Received: " + JSON.stringify(msg.data));
+});
+
+console.log("Press <enter> to publish a message. Use <ctrl>-c to stop.");
+
+/* Publish a message when enter is pressed */
+process.stdin.resume();
+process.stdin.on('data', function (chunk) {
+  if (chunk.toString() === "\n") {
+    channel.publish("update", { "team": "Man United" });
+  }
 });
