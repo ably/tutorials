334016a9ca5e70f886531349fe5afe0d4f455b98
Matthew O'Riordan <matthew.oriordan@gmail.com>
2016-09-02 17:10:22 +0100

    Step 3 - Subscribe to messages

--- example.js
+++ example.js
@@ -5,3 +5,9 @@ if (ApiKey.indexOf("INSERT") === 0) { throw("Cannot run without an API key. Add 
 
 /* Instance the Ably library */
 var realtime = new Ably.Realtime({ key: ApiKey });
+
+/* Subscribe to messages on the sport channel */
+var channel = realtime.channels.get("sport");
+channel.subscribe(function(msg) {
+  console.log("Received: " + JSON.stringify(msg.data));
+});
