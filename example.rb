c99d1054f2f630ee835c409d91d532812daeb433
Matthew O'Riordan <matthew.oriordan@gmail.com>
2016-09-03 17:39:49 +0100

    Step 4 - Publishing messages to be stored with channel history

--- example.rb
+++ example.rb
@@ -5,3 +5,9 @@ raise "Cannot run without an API key. Add your key to example.rb" if API_KEY.inc
 
 # Instance the Ably library
 ably = Ably::Rest.new(key: API_KEY)
+
+puts "Publishing three messages..."
+channel = ably.channels.get('persisted:sounds')
+channel.publish 'play', 'bark'
+channel.publish 'play', 'meow'
+channel.publish 'play', 'cluck'
