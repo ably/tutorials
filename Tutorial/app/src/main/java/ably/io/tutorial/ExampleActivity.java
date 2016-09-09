e8412f5976cd72b58078fcd5d97276ca3ea6f30f
Matthew O'Riordan <matthew.oriordan@gmail.com>
2016-09-09 22:22:44 +0100

    Step 5a - Set up environment

---
+++ Tutorial/app/src/main/java/ably/io/tutorial/ExampleActivity.java
@@ -0,0 +1,13 @@
+package ably.io.tutorial;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example);
+    }
+}
