0e9e6db0ddc1662e4cc9291c81bf3890fab85ebe
Matthew O'Riordan <matthew.oriordan@gmail.com>
2016-09-14 23:58:31 +0100

    Step 3 - Set up your web server

--- server.rb
+++ server.rb
@@ -1,7 +1,12 @@
 require 'ably'
+require 'sinatra'
 
 API_KEY = "INSERT-API-KEY-HERE" # Add your API key here
 raise "Cannot run without an API key. Add your key to example.rb" if API_KEY.include?('INSERT')
 
 # Instance the Ably server REST library
 ably = Ably::Rest.new(key: API_KEY)
+
+get '/' do
+  'Hello, I am a very simple server'
+end
