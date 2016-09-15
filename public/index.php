90de4e66ae8aac378eceb5854d9d2297e3106a34
Bartlomiej Wereszczynski <bartlomiej.wereszczynski@10clouds.com>
2016-09-15 14:21:02 +0200

    4. Publishing messages to be stored with channel history

--- public/index.php
+++ public/index.php
@@ -3,3 +3,17 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 throw new \RuntimeException('Insert your API key'); // REMOVE THIS
 $ably = new \Ably\AblyRest("{{ApiKey}}");
+
+echo "<pre>\n";
+
+echo "Retrieving channel...\n";
+
+$channel = $ably->channels->get("persisted:sounds");
+
+echo "Publishing messages...\n";
+
+$channel->publish("play","bark");
+$channel->publish("play","meow");
+$channel->publish("play","cluck");
+
+echo "</pre>\n";
