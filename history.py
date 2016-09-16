d71f7ac9c115f48e89d927ef56cfd4c297f3a3ab
Bartlomiej Wereszczynski <bartlomiej.wereszczynski@10clouds.com>
2016-09-16 14:51:14 +0200

    4. Publishing messages to be stored with channel history

--- history.py
+++ history.py
@@ -2,3 +2,13 @@ from __future__ import unicode_literals
 from ably import AblyRest
 
 client = AblyRest('{{ApiKey}}')
+
+print('Retrieving channel...')
+
+channel = client.channels.get('persisted:sounds')
+
+print('Publishing messages...')
+
+channel.publish('play', 'bark')
+channel.publish('play', 'meow')
+channel.publish('play', 'cluck')
