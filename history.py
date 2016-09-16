e8e4e8d990ad4bb398e7b9f0dd23aabbcee7811f
Bartlomiej Wereszczynski <bartlomiej.wereszczynski@10clouds.com>
2016-09-16 14:51:50 +0200

    5. Retrieving messages from history

--- history.py
+++ history.py
@@ -12,3 +12,19 @@ print('Publishing messages...')
 channel.publish('play', 'bark')
 channel.publish('play', 'meow')
 channel.publish('play', 'cluck')
+
+print('Releasing channel...')
+# this part is only for tutorial purposes, in real you can use same instance of $channel
+del channel
+client.channels.release('persisted:sounds')
+
+print('Retrieving channel...')
+
+channel = client.channels.get('persisted:sounds')
+
+print('Retrieving messages...')
+
+paginatedResult = channel.history()
+
+for message in paginatedResult.items:
+    print('Last message published: {}'.format(message.data))
