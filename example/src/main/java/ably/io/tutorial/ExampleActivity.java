dce929fef19a0e368cb223ec8270498429c11b35
piotr.kazmierczak <piotr.kazmierczak@10clouds.com>
2016-09-16 14:36:40 +0200

    Step 3 - Subscribe to messages

--- example/src/main/java/ably/io/tutorial/ExampleActivity.java
+++ example/src/main/java/ably/io/tutorial/ExampleActivity.java
@@ -2,9 +2,13 @@ package ably.io.tutorial;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import io.ably.lib.realtime.AblyRealtime;
+import io.ably.lib.realtime.Channel;
 import io.ably.lib.types.AblyException;
+import io.ably.lib.types.Message;
 
 public class ExampleActivity extends AppCompatActivity {
 
@@ -17,10 +21,14 @@ public class ExampleActivity extends AppCompatActivity {
         }
     }
 
+    private Channel channel;
+    private EditText messageEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_example);
+        messageEditText = (EditText) findViewById(R.id.etMessage);
         try {
             initAbly();
         } catch (AblyException e) {
@@ -31,5 +39,14 @@ public class ExampleActivity extends AppCompatActivity {
     /* Add AblyException to method signature as AblyRealtime constructor can throw one */
     private void initAbly() throws AblyException {
         AblyRealtime realtime = new AblyRealtime(API_KEY);
+        /* Get  sport channel you can subscribe to */
+        channel = realtime.channels.get("sport");
+        channel.subscribe(new Channel.MessageListener() {
+            @Override
+            public void onMessage(Message messages) {
+                /* show a toast when message is received */
+                Toast.makeText(getBaseContext(), "Message received: " + messages.data, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
