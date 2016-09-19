5089b506a6f1b66dff196d8828b9df981ad4dfc8
ShelinHime <Hime@MacBook-Pro-Joanna.local>
2016-09-19 17:50:42 +0200

    Initial commit
    Initial commit

---
+++ example/example/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  example
+//
+//  Created by Joanna Kubiak on 19.09.2016.
+//  Copyright © 2016 Ably. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ example/example/ExampleViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ExampleViewController.swift
+//  example
+//
+//  Created by Joanna Kubiak on 19.09.2016.
+//  Copyright © 2016 Ably. All rights reserved.
+//
+
+import UIKit
+
+class ExampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func subscribeAction(sender: AnyObject) {
+    }
+
+    @IBAction func publishAction(sender: AnyObject) {
+    }
+
+}
