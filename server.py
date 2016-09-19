91ec78ee208283dbb7829b74a71c9c5ea9a9dde2
Bartlomiej Wereszczynski <bartlomiej.wereszczynski@10clouds.com>
2016-09-19 15:09:37 +0200

    3. Set up your web server

--- server.py
+++ server.py
@@ -1,6 +1,22 @@
 from __future__ import unicode_literals
 from ably import AblyRest
+import web
 
 raise RuntimeError('Insert your API key')  # REMOVE THIS
 
 client = AblyRest('{{ApiKey}}')
+
+
+class index:
+    def GET(self):
+        return 'Hello, I am a very simple server'
+
+
+urls = (
+    '/', index
+)
+
+app = web.application(urls, globals())
+
+if __name__ == "__main__":
+    app.run()
