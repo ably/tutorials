bbb7f6c1f06d3171963201a999a4f52d6992b838
Bartlomiej Wereszczynski <bartlomiej.wereszczynski@10clouds.com>
2016-09-19 08:00:28 +0200

    3. Set up your web server

--- server.py
+++ server.py
@@ -1,4 +1,20 @@
 from __future__ import unicode_literals
 from ably import AblyRest
+import web
 
 client = AblyRest('{{ApiKey}}')
+
+
+class index:
+    def GET(self):
+        return 'Hello, I am a very simple server'
+
+
+urls = (
+    '/', index
+)
+
+app = web.application(urls, globals())
+
+if __name__ == "__main__":
+    app.run()
