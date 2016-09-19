fbf5c4528183dc3f0058faccf676f363b5f1ee4f
Bartlomiej Wereszczynski <bartlomiej.wereszczynski@10clouds.com>
2016-09-19 16:13:05 +0200

    6. Authenticating logged in users

--- auth.php
+++ auth.php
@@ -1,5 +1,24 @@
 <?php
 require_once 'ably.php';
+// Check if the user is logged in
+if (isset($_COOKIE['username'])) {
+    // Issue a token request with pub & sub permissions on all channels +
+    // configure the token with an identity
+    $tokenParams = [
+        'capability' => [
+            '*' => ['publish', 'subscribe']
+        ],
+        'clientId'  => $_COOKIE['username']
+    ];
+} else {
+    // Issue a token with subscribe privileges restricted to one channel
+    // and configure the token without an identity (anonymous)
+    $tokenParams = [
+        'capability' => [
+            'notifications' => ['subscribe']
+        ]
+    ];
+}
 header('Content-Type: application/json');
 # Issue token requests to clients sending a request to the /auth.php endpoint
-echo json_encode($ably->auth->createTokenRequest()->toArray());
+echo json_encode($ably->auth->createTokenRequest($tokenParams)->toArray());
