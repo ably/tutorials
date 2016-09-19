f949bcf27a99f527201cb7f297561f4bdfedebd3
Bartlomiej Wereszczynski <bartlomiej.wereszczynski@10clouds.com>
2016-09-19 16:10:00 +0200

    5. Setting up a client to use token authentication

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
-<?php
-echo "Hello, I am a very simple server";
+<html>
+    <head>
+        <script src="https://cdn.ably.io/lib/ably.min-1.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <h1>Token auth example</h1>
+    </body>
+    <script type="text/javascript">
+        /* Set up a Realtime client that authenticates with the local web server auth endpoint */
+        var realtime = new Ably.Realtime({ authUrl: '/auth.php' });
+        realtime.connection.once('connected', function() {
+            alert("We're connected using the token request from the server /auth.php endpoint!");
+        });
+    </script>
+</html>
