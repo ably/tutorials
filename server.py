6a1ccb4cc734c7cdbbcd858dac097feb08381ef8
Bartlomiej Wereszczynski <bartlomiej.wereszczynski@10clouds.com>
2016-09-19 15:14:23 +0200

    6. Authenticating logged in users

--- server.py
+++ server.py
@@ -16,13 +16,45 @@ class index:
 
 class auth:
     def GET(self):
+        username = web.cookies().get('username')
+        if username is not None:
+            token_params = {
+                'capability': {
+                    '*': ['publish', 'subscribe']
+                },
+                'client_id': username
+            }
+        else:
+            token_params = {
+                'capability': {
+                    'notifications': ['subscribe']
+                }
+            }
         web.header('Content-Type', 'application/json')
-        return json.dumps(client.auth.create_token_request().to_dict())
+        return json.dumps(client.auth.create_token_request(token_params).to_dict())
+
+
+class login:
+    def GET(self):
+        username = web.input().get('username')
+        if username is not None:
+            web.setcookie('username', username)
+            raise web.seeother('/')
+        else:
+            raise web.InternalError('Username is required to login')
+
+
+class logout:
+    def GET(self):
+        web.setcookie('username', None, -1)
+        raise web.seeother('/')
 
 
 urls = (
     '/', index,
-    '/auth', auth
+    '/auth', auth,
+    '/login', login,
+    '/logout', logout
 )
 
 app = web.application(urls, globals())
