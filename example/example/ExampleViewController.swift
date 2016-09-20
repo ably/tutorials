f3b6798fdf393f61fe0a94b6064b7092687da499
ShelinHime <Hime@MacBook-Pro-Joanna.local>
2016-09-20 15:21:46 +0200

    Initial commit

---
+++ example/example/ExampleViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ExampleViewController.swift
+//  example
+//
+//  Created by Joanna Kubiak on 20.09.2016.
+//  Copyright © 2016 Ably. All rights reserved.
+//
+
+import UIKit
+
+class ExampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func publishAction(sender: AnyObject) {
+    }
+
+    @IBAction func retriveHistoryAction(sender: AnyObject) {
+    }
+
+}
