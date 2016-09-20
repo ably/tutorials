ce34b05f0916cb3b507873e8cd71f7a1924801d0
ShelinHime <Hime@MacBook-Pro-Joanna.local>
2016-09-20 15:33:12 +0200

    Step 3 - Install Ably

--- example/example/ExampleViewController.swift
+++ example/example/ExampleViewController.swift
@@ -7,11 +7,18 @@
 //
 
 import UIKit
+import AblyRealtime
 
 class ExampleViewController: UIViewController {
 
+    private let API_KEY = "INSERT-YOU-API-KEY-HERE" /* Add your API key here */
+
+    private var client: ARTRealtime!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        /* Instance the Ably library */
+        client = ARTRealtime(key: API_KEY)
     }
 
     @IBAction func publishAction(sender: AnyObject) {
