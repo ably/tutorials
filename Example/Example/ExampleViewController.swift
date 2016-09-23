ed24f3e2dc25cd919c2b50e2fb6e16c533fc7197
ShelinHime <Hime@MacBook-Pro-Joanna.local>
2016-09-23 12:22:45 +0200

    Step 3a) Set up Xcode project

---
+++ Example/Example/ExampleViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleViewController.swift
+//  Example
+//
+//  Created by Joanna Kubiak on 22.09.2016.
+//  Copyright © 2016 example. All rights reserved.
+//
+
+import UIKit
+
+class ExampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
