59dcc5694f011b4f244837da57d95fec9f5b1679
ShelinHime <Hime@MacBook-Pro-Joanna.local>
2016-09-23 13:34:24 +0200

    Step 2 - Install Ably

--- Example/Example/ExampleViewController.swift
+++ Example/Example/ExampleViewController.swift
@@ -7,11 +7,43 @@
 //
 
 import UIKit
+import AblyRealtime
 
 class ExampleViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    private let API_KEY = "INSERT-YOUR-API-KEY-HERE" /* Add your API key here */
+
+    private var client: ARTRealtime!
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+
+        /*Show alert if API_KEY is not set properly*/
+        guard API_KEY.rangeOfString("INSERT") == nil else {
+            return showAlert("Error", message: "You have not set the API key, please configure the API_KEY in ExampleViewController.swift to run this demo.")
+        }
+
+        /* Instance the Ably library */
+        client = ARTRealtime(key: API_KEY)
+        client.connection.on { state in
+            if let state = state {
+                switch state.current {
+                case .Connected:
+                    self.showAlert("Connected", message: "Successfully connected to API.")
+                case .Failed:
+                    self.showAlert("Failed", message: "There was a problem connecting to API")
+                default:
+                    break
+                }
+            }
+        }
+    }
+
+    private func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+
+        self.presentViewController(alertController, animated: true, completion: nil)
     }
 
 }
