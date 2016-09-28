90382f680c7c8df4f71882057d7cfdcb9b0c5ac2
Bartlomiej Wereszczynski <bartlomiej.wereszczynski@10clouds.com>
2016-09-28 08:22:55 +0200

    4. Publish a message

--- index.php
+++ index.php
@@ -7,13 +7,14 @@ require_once 'ably.php';
 </head>
 <body>
 <h1>Publish &amp; Subscribe sample</h1>
+<iframe src="publish.php"></iframe>
 </body>
 <script type="text/javascript">
     /* Set up a Realtime client which will subscribe to 'sport' channel*/
     var realtime = new Ably.Realtime('<?= $apikey?>');
     var channel = realtime.channels.get("sport");
     channel.subscribe(function(msg) {
-        alert("Received: " + JSON.stringify(msg.data));
+        alert("Received: " + msg.data);
     });
 </script>
 </html>
