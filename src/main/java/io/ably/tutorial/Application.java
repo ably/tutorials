c2f1ab39bd38f5c4ee71cf90a8dfbdb63e5283dd
piotr.kazmierczak <piotr.kazmierczak@10clouds.com>
2016-10-21 11:34:05 +0200

    Step 5 – Setting up a client to use token authentication

--- src/main/java/io/ably/tutorial/Application.java
+++ src/main/java/io/ably/tutorial/Application.java
@@ -3,11 +3,22 @@ package io.ably.tutorial;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
 @SpringBootApplication
-public class Application {
+public class Application extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    }
+
+    /* Add ViewController so '/' URL would forward us to index.html file */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+        super.addViewControllers(registry);
     }
 }
