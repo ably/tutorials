4828df8369c6221f461d479a926f1f507165343a
piotr.kazmierczak <piotr.kazmierczak@10clouds.com>
2016-10-21 11:33:43 +0200

    Step 4 – Respond to authentication requests

--- src/main/java/io/ably/tutorial/Example.java
+++ src/main/java/io/ably/tutorial/Example.java
@@ -1,9 +1,14 @@
 package io.ably.tutorial;
 
+import com.google.gson.Gson;
 import io.ably.lib.rest.AblyRest;
+import io.ably.lib.rest.Auth;
 import io.ably.lib.types.AblyException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class Example {
@@ -30,6 +35,20 @@ public class Example {
     /* Add AblyException to method signature as AblyRest constructor can throw one */
     private void initAbly() throws AblyException {
         ablyRest = new AblyRest(API_KEY);
+    }
+
+    /* Issue token requests to clients sending a request to the /auth endpoint */
+    @RequestMapping("/auth")
+    public String auth(HttpServletRequest request, HttpServletResponse response) {
+        Auth.TokenParams tokenParams = new Auth.TokenParams(); /* Use token defaults for now */
+        try {
+            Auth.TokenRequest tokenRequest = ablyRest.auth.createTokenRequest(null, tokenParams);
+            response.setHeader("Content-Type", "application/json");
+            return new Gson().toJson(tokenRequest);
+        } catch (AblyException e) {
+            response.setStatus(500);
+            return "Error requesting token: " + e.getMessage();
+        }
     }
 
     /* Add method index with RequestMapping annotation so the web app knows which method to use on our default URL */
