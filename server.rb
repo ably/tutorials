d99a64c91aedc2151b5d81a80bbc5d69f7f4c85c
ShelinHime <Hime@MacBook-Pro-Joanna.local>
2016-10-22 12:45:23 +0200

    Step 4 - Respond to authentication requests

--- server.rb
+++ server.rb
@@ -1,4 +1,5 @@
 require 'ably'
+require 'json'
 require 'sinatra'
 
 API_KEY = "INSERT-API-KEY-HERE" # Add your API key here
@@ -9,4 +10,10 @@ ably = Ably::Rest.new(key: API_KEY)
 
 get '/' do
   'Hello, I am a very simple server'
+end
+
+# Issue token requests to clients sending a request to the /auth endpoint
+get '/auth' do
+  content_type :json
+  ably.auth.create_token_request.to_json
 end
