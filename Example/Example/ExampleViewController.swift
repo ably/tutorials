3ae450dc9d729be2243a735a3c48bb21486e9930
ShelinHime <Hime@MacBook-Pro-Joanna.local>
2016-10-22 13:24:52 +0200

    Step 5b) Install Ably and setup client to use token authentication

--- Example/Example/ExampleViewController.swift
+++ Example/Example/ExampleViewController.swift
@@ -7,11 +7,45 @@
 //
 
 import UIKit
+import Ably
 
 class ExampleViewController: UIViewController {
+
+    private var client: ARTRealtime!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        connectToAbly()
+    }
+
+    private func connectToAbly() {
+        /* Set up a Realtime client that authenticates with the local web server auth endpoint */
+        let options = ARTClientOptions()
+        options.authUrl = NSURL(string: "http://localhost:4567/auth")
+
+        /* Instance the Ably library */
+        client = ARTRealtime(options: options)
+        client.connection.on { state in
+            if let state = state {
+                switch state.current {
+                case .Connected:
+                    self.showAlert("Connected", message: "Successfully connected to API.")
+                case .Failed:
+                    print( self.client.connection.errorReason)
+                    self.showAlert("Failed", message: "There was a problem connecting to API")
+                default:
+                    break
+                }
+            }
+        }
+
+    }
+
+    private func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+
+        self.presentViewController(alertController, animated: true, completion: nil)
     }
 }
