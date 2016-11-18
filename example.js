5a3959488c2519a0ce84e87d1e32c789dd321e08
Simon Woolf <simon@ably.io>
2016-11-18 18:39:11 +0000

    Step 3 - Enter the presence set

--- example.js
+++ example.js
@@ -6,3 +6,14 @@ if (ApiKey.indexOf("INSERT") === 0) { throw("Cannot run without an API key. Add 
 
 /* Instance the Ably library */
 var realtime = new Ably.Realtime({ key: ApiKey, clientId: clientId });
+
+/* Enter the presence set of the 'chatroom' channel */
+var channel = realtime.channels.get('chatroom');
+channel.attach(function(err) {
+  if(err) { return console.log("Error attaching to the channel"); }
+  console.log('We are now attached to the channel');
+  channel.presence.enter('hello', function(err) {
+    if(err) { return console.error("Error entering presence"); }
+    console.log('We are now successfully present');
+  });
+});
