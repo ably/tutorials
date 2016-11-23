7105d2b3a84ab0f9ab2db2d3856ff60ea2fb9ce4
Simon Woolf <simon@ably.io>
2016-11-23 17:17:09 +0000

    Step 4 - interoperability between Pusher and Ably client libraries

--- example.js
+++ example.js
@@ -1,3 +1,4 @@
+const Ably = require("ably");
 const Pusher = require("pusher");
 
 const API_KEY = 'appid.keyid:keysecret'; // Replace this with your API key
@@ -10,12 +11,39 @@ const APP_ID = API_KEY.split('.')[0],
   KEY_NAME = KEY_PARTS[0],
   KEY_SECRET = KEY_PARTS[1];
 
+const pusherChannelName = 'some_channel';
+const ablyChannelName = 'public:' + pusherChannelName;
 
-/* Instance the Pusher library */
+/* ABLY CLIENT LIB */
+/* Instance the Ably library */
+const ably = new Ably.Realtime(API_KEY);
+
+/* Subscribe to the 'some_channel' channel with the Ably client */
+const ablyChannel = ably.channels.get(ablyChannelName);
+ablyChannel.subscribe(function(message) {
+  console.log('Ably client received a message: ' + message.name + ', data: ' + JSON.stringify(message.data));
+});
+
+/* PUSHER CLIENT LIB */
+/* Instance the Pusher node library */
 const pusher = new Pusher({
   appId     : APP_ID,
   key       : KEY_NAME,
   secret    : KEY_SECRET,
   host      : 'rest-pusher.ably.io',
   encrypted : true
+});
+
+
+console.log("Press p<enter> to publish a message with the Pusher nodejs client library, and see it be received by the Ably client library. Press q<enter> to quit.");
+process.stdin.resume();
+process.stdin.on('data', function (chunk) {
+  switch(chunk.toString()) {
+    case "p\n":
+      pusher.trigger(pusherChannelName, 'eventName', { 'Some': 'JSON data' });
+      break;
+    case "q\n":
+      ably.close();
+      process.exit();
+  }
 });
