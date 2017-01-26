96b5a92dfb89887bff4d885bbb90c7925f091448
Matthew O'Riordan <matthew.oriordan@gmail.com>
2017-01-26 15:37:53 +0000

    Step 4 - Plug in Ably client-side, update UI, auth

--- public/app.js
+++ public/app.js
@@ -1,6 +1,48 @@
 $(function() {
-  var $actionButton = $('#action-btn');
-  $actionButton.on('click', function() {
-    alert("Not yet implemented");
+  /* Set up realtime library and authenticate using token issued from server */
+  var ably = new Ably.Realtime({ authUrl: '/auth' });
+  var filteredChannel = ably.channels.get('neutrino:filtered');
+  var publishChannel = ably.channels.get('neutrino:raw');
+
+  var $output = $('#output'),
+      $status = $('#status'),
+      $actionButton = $('#action-btn'),
+      $text = $('#text');
+
+  /* Subscribe to filtered text published on this channel */
+  filteredChannel.subscribe(function(message) {
+    var $filtered = $('<div class="filtered">').text(message.data.filtered);
+    var $stat = $('<div class="stat">').text("Neutrino took " + message.data.neutrinoTime + "ms");
+    $output.prepend($('<div>').append($stat).append($filtered));
   });
+
+  $actionButton.on('click', function() {
+    var text = $text.val();
+    if (text.replace(' ') != '') {
+      /* Publish text to the Ably channel so that the queue worker receives it via queues */
+      publishChannel.publish('text', text, function(err) {
+        if (err) {
+          showStatus('Failed to publish text!');
+          $text.val(text);
+          return;
+        }
+        clearStatus();
+      });
+      showStatus('Sending unfiltered bad text...');
+      $text.val('');
+    }
+  });
+
+  ably.connection.on('connecting', function() { showStatus('Connecting to Ably...'); });
+  ably.connection.on('connected', function() { clearStatus(); });
+  ably.connection.on('disconnected', function() { showStatus('Disconnected from Ably...'); });
+  ably.connection.on('suspended', function() { showStatus('Disconnected from Ably for a while...'); });
+
+  function showStatus(text) {
+    $status.text(text).show();
+  }
+
+  function clearStatus() {
+    $status.fadeOut(750);
+  }
 });
