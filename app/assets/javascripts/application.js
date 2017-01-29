4b3548a3afbd48c46a62ce212a7b844a7c2be159
Matthew O'Riordan <matthew.oriordan@gmail.com>
2017-01-29 11:49:23 +0000

    Step 5 - Plug in Ably client-side and update UI

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,8 +3,48 @@
 //= require_tree .
 
 $(function() {
-  var $actionButton = $('#action-btn');
-  $actionButton.on('click', function() {
-    alert("Not yet implemented");
+  /* Set up realtime library and authenticate using token issued from server */
+  var ably = new Ably.Realtime({ authUrl: '/auth' });
+  var publishChannel = ably.channels.get('chuck:ask');
+  var jokeChannel = ably.channels.get('chuck:jokes');
+
+  var $output = $('#output'),
+      $status = $('#status'),
+      $actionButton = $('#action-btn'),
+      $text = $('#text');
+
+  /* Subscribe to jokes published on this channel */
+  jokeChannel.subscribe(function(message) {
+    var $joke = $('<div class="joke">').text(message.data.joke);
+    var $stat = $('<div class="stat">').text("Chuck's API took " + message.data.chuckTime + "ms");
+    $output.prepend($('<div>').append($stat).append($joke));
   });
+
+  $actionButton.on('click', function() {
+    var text = $text.val();
+    /* Publish text to the Ably channel so that the WebHook is triggered and delivered to the Rails app */
+    publishChannel.publish('text', text, function(err) {
+      if (err) {
+        showStatus('Failed to publish request to Chuck!');
+        $text.val(text);
+        return;
+      }
+      clearStatus();
+    });
+    showStatus('Sending request to Chuck...');
+    $text.val('');
+  });
+
+  ably.connection.on('connecting', function() { showStatus('Connecting to Ably...'); });
+  ably.connection.on('connected', function() { clearStatus(); });
+  ably.connection.on('disconnected', function() { showStatus('Disconnected from Ably...'); });
+  ably.connection.on('suspended', function() { showStatus('Disconnected from Ably for a while...'); });
+
+  function showStatus(text) {
+    $status.text(text).show();
+  }
+
+  function clearStatus() {
+    $status.fadeOut(750);
+  }
 });
