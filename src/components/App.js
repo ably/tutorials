d04dcb5608d760b48a34033bd5f413282a86ca60
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-10-28 03:17:04 +0100

    Step 12 – Create App Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CommentBox from './CommentBox';
+import Comments from './Comments';
+import Ably from '../ably';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        comments: []
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-half is-offset-one-quarter">
+              <CommentBox />
+              <Comments comments={this.state.comments} />
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
