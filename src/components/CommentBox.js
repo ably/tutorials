bf5c81508970fbe571437ded6b4da084adfae70a
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-11-28 14:12:33 +0100

    Step 8 – Create CommentBox Component

---
+++ src/components/CommentBox.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class CommentBox extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="title">Kindly leave your thoughts below</h1>
+        <form onSubmit={this.addComment}>
+          <div className="field">
+            <div className="control">
+              <input type="text" className="input" name="name" placeholder="Your name" />
+            </div>
+          </div>
+          <div className="field">
+            <div className="control">
+              <textarea className="textarea" name="comment" placeholder="Add a comment"></textarea>
+            </div>
+          </div>
+          <div className="field">
+            <div className="control">
+              <button className="button is-primary">Submit</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default CommentBox;
