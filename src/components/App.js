4af2580cc7d79063763a446b913c08e33c3ba449
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-11-28 14:19:34 +0100

    Step 12 – Create App Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import CommentBox from './CommentBox';
+import Comments from './Comments';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: []
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-half is-offset-one-quarter">
+              <CommentBox />
+              <Comments comments={this.state.comments.reverse()} />
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
