f48f3ed5f93fad66be4785af1badb60f490407f5
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-11-28 14:10:44 +0100

    Step 7 – Install Ably

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+require('./ably');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
