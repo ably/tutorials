c5edf743cb8774d084eb79378181f8f7dad62372
samuel ogundipe <ayoogundipe2005@gmail.com>
2017-12-01 23:52:59 +0100

    Added load_messages function to chat.js

--- Ably_presence_publish_subscribe_react_native/Chat.js
+++ Ably_presence_publish_subscribe_react_native/Chat.js
@@ -79,6 +79,70 @@ export default class Chat extends Component<{}> {
     });
   };
 
+  load_messages = () => {
+    var chat = [];
+    for (var i = 0; i < this.state.msg.length; i++) {
+      if (this.state.msg[i].action == "joined") {
+        chat.push(
+          <Text style={{ padding: 3 }} key={i}>
+            {this.state.msg[i].user} {this.state.msg[i].msg}
+          </Text>
+        );
+      } else if (this.state.msg[i].action == "left") {
+        chat.push(
+          <Text style={{ padding: 3 }} key={i}>
+            {this.state.msg[i].user} {this.state.msg[i].msg}
+          </Text>
+        );
+      } else if (this.state.msg[i].user == this.state.user) {
+        chat.push(
+          <View
+            key={i}
+            style={{
+              width: "auto",
+              backgroundColor: "rgb(244, 226, 96)",
+              alignSelf: "flex-start",
+              padding: 10,
+              borderRadius: 25,
+              marginBottom: 5
+            }}
+          >
+            <Text> {this.state.msg[i].msg}  </Text>
+          </View>
+        );
+      } else {
+        chat.push(
+          <View key={i}>
+            <View
+              style={{
+                width: "auto",
+                backgroundColor: "rgb(125, 185, 232)",
+                alignSelf: "flex-end",
+                padding: 10,
+                borderRadius: 25
+              }}
+            >
+              <Text>
+                {" "}{this.state.msg[i].msg}{" "}
+              </Text>
+            </View>
+            <Text
+              style={{
+                width: "auto",
+                alignSelf: "flex-end",
+                paddingRight: 10,
+                marginBottom: 5,
+                fontSize: 6
+              }}
+            >
+              {this.state.msg[i].user}
+            </Text>
+          </View>
+        );
+      }
+    }
+    return chat;
+  };
   render = () => {
     return <View style={styles.container} />;
   };
