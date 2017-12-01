8139cbe396aae54ca18a917ac709df4a0876f55f
samuel ogundipe <ayoogundipe2005@gmail.com>
2017-12-02 00:04:16 +0100

    Added load_messages function to chat.js. added text import

--- Ably_presence_publish_subscribe_react_native/Chat.js
+++ Ably_presence_publish_subscribe_react_native/Chat.js
@@ -5,7 +5,7 @@
  */
 
 import React, { Component } from "react";
-import { StyleSheet, View, AsyncStorage } from "react-native";
+import { StyleSheet, View, Text, AsyncStorage } from "react-native";
 
 var realtime = require("ably").Realtime;
 var ably, channel;
