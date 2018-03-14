c52604687c9182caa7bffab3a0a332541df4d454
tomC <tc13270@my.bristol.ac.uk>
2018-03-14 10:59:29 +0000

    Step 4 - Detect key presses

--- mqtt-snake/controller.js
+++ mqtt-snake/controller.js
@@ -1,2 +1,13 @@
 const mqtt = require('mqtt');
 const keypress = require('keypress');
+
+keypress(process.stdin);
+process.stdin.setRawMode(true);
+process.stdin.on('keypress', function (ch, key) {
+  if (key) {
+    console.log('Key clicked: ' +  key.name);
+    if (key.ctrl && (key.name == 'c')) {
+      process.stdin.pause();
+    }
+  }
+});
