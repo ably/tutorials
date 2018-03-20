20cf8b4d581554a3edbb2f3a430dda3b5a21b6fa
tomC <tc13270@my.bristol.ac.uk>
2018-03-20 15:14:50 +0000

    Step 4 - Connect to Ably through MQTT

--- mqtt-snake/controller.go
+++ mqtt-snake/controller.go
@@ -4,3 +4,17 @@ import (
   MQTT "github.com/eclipse/paho.mqtt.golang"
   termbox "github.com/nsf/termbox-go"
 )
+
+func main() {
+  options := MQTT.NewClientOptions();
+  options.AddBroker("ssl://mqtt.ably.io:8883")
+  options.SetKeepAlive(15)
+  options.SetUsername("FIRST_HALF_OF_API_KEY")
+  options.SetPassword("SECOND_HALF_OF_API_KEY")
+  client := MQTT.NewClient(options)
+  if token := client.Connect(); token.Wait() && token.Error() != nil {
+    panic(token.Error())
+  } else {
+    fmt.Println("Connected!")
+  }
+}
