8e48ec6ee4b4f36ff5df3eb59bb3bc974945123b
tomC <tc13270@my.bristol.ac.uk>
2018-03-20 12:23:07 +0000

    Step 4 - Connect to Ably through MQTT

--- mqtt-snake/controller.js
+++ mqtt-snake/controller.js
@@ -1,2 +1,16 @@
 const mqtt = require('mqtt');
 const keypress = require('keypress');
+
+var options = { keepAlive: 15,
+  username: 'FIRST_HALF_OF_API_KEY',
+  password: 'SECOND_HALF_OF_API_KEY',
+  port: 8883
+};
+var client = mqtt.connect('mqtts:mqtt.ably.io', options);
+client.on('connect', function () {
+  console.log('connected!');
+});
+client.on('error', function(err){
+  console.log(err);
+  client.end();
+});
