e874038edfc52745b5deb85d53de29fc98f98376
tomC <tc13270@my.bristol.ac.uk>
2018-03-21 10:39:34 +0000

    Step 4 - Connect to Ably through MQTT

--- mqtt-snake/controller.py
+++ mqtt-snake/controller.py
@@ -1,2 +1,15 @@
 import paho.mqtt.client as mqtt
 import curses
+
+def on_connect(client, userdata, flags, rc):
+  print('Connected')
+def on_disconnect(client, userdata, rc):
+  print('Disconnected')
+  client.loop_stop()
+client = mqtt.Client()
+client.username_pw_set('FIRST_HALF_OF_API_KEY', 'SECOND_HALF_OF_API_KEY')
+client.tls_set()
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.loop_start()
+client.connect('mqtt.ably.io', port=8883, keepalive=15)
