23b5939f13f4e3249b73f074bdfa2643a573259e
samuel ogundipe <ayoogundipe2005@gmail.com>
2018-07-12 18:18:30 +0100

    sending files - file transfers

--- WebRTC-file-transfers-with-Ably/ably-datachannel.js
+++ WebRTC-file-transfers-with-Ably/ably-datachannel.js
@@ -43,3 +43,41 @@ function renderMembers() {
     }
     list.innerHTML = html
 }
+
+function PrepareChat(client_id) {
+    if (client_id === clientId) return
+        // Create a new connection
+    currentChat = client_id
+    if (!connections[client_id]) {
+        connections[client_id] = new Connection(client_id, AblyRealtime, true)
+    }
+    document.getElementById('chat').style.display = 'block'
+    render()
+}
+AblyRealtime.subscribe(`rtc-signal/${clientId}`, msg => {
+    if (!connections[msg.data.user]) {
+        connections[msg.data.user] = new Connection(msg.data.user, AblyRealtime, false)
+    }
+    connections[msg.data.user].handleSignal(msg.data.signal)
+})
+
+function sendMessage() {
+    if (!messageList[currentChat]) {
+        messageList[currentChat] = {}
+    }
+    var MAX_FSIZE = 2.0
+    var fileInput = document.getElementById('file')
+    var file = fileInput.files[0];
+    var fileReader = new FileReader();
+    if (file) {
+        var mbSize = file.size / (1024 * 1024);
+        if (mbSize > MAX_FSIZE) {
+            alert("Your file is too big, sorry.");
+            // Reset file input
+            return;
+        }
+        fileReader.onload = (e) => { onReadAsDataURL(e, null, file) }
+        fileReader.readAsDataURL(file)
+        render()
+    }
+}
