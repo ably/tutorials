d174ca97b9f4ef132381a4b34548100f1e4d2eb8
Srushtika <n.srushtika@gmail.com>
2018-11-02 15:25:30 +0000

    Step 5 - Demo buttons functionality

--- main.js
+++ main.js
@@ -22,3 +22,49 @@ metaChannel.subscribe('channel.occupancy', (msg) => {
         resultArea.scrollTop = resultArea.scrollHeight;
     }
 })
+
+function addPublisherInstance() {
+    resultArea.value += ('\n[LOCAL LOG - ' + (new Date().toLocaleTimeString()) + ' ]: Adding new publisher instance\n')
+    var myKey = '<YOUR-API-KEY>'
+    var ably = new Ably.Realtime({
+        key: myKey
+    });
+    var regularChannel = ably.channels.get("regular-channel")
+    console.log('adding publisher instance')
+    regularChannel.publish('test-data', {
+        data: "Dummy Data",
+        time: Date.now()
+    })
+}
+
+function addSubscriberInstance() {
+    resultArea.value += ('\n[LOCAL LOG - ' + (new Date().toLocaleTimeString()) + ' ]: Adding new subscriber instance\n')
+    var myKey = '<YOUR-API-KEY>'
+    var ably = new Ably.Realtime({
+        key: myKey
+    });
+    var regularChannel = ably.channels.get("regular-channel")
+    console.log('adding subscriber instance')
+    regularChannel.subscribe('test-data', (data) => {
+        //do whatever
+        console.log('Subscription working')
+    })
+}
+
+function enterPresence() {
+    resultArea.value += ('\n[LOCAL LOG - ' + (new Date().toLocaleTimeString()) + ' ]: Entering presence\n')
+    var ably = new Ably.Realtime({
+        key: apiKey
+    });
+    var regularChannel = ably.channels.get("regular-channel")
+    regularChannel.presence.enter();
+}
+
+function leavePresence() {
+    resultArea.value += ('\n[LOCAL LOG - ' + (new Date().toLocaleTimeString()) + ' ]: Leaving presence\n')
+    var ably = new Ably.Realtime({
+        key: apiKey
+    });
+    var regularChannel = ably.channels.get("regular-channel")
+    regularChannel.presence.leave();
+}
