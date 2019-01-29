fecb121e4b66cbb6e3a8ce632f393f18179bf328
Christopher Batin <chris@batin.me>
2019-01-29 18:52:54 +0000

    Added testing

--- testappserver/index.js
+++ testappserver/index.js
@@ -61,3 +61,65 @@ function subscribeDevice (device){
       }
   })
 }
+
+app.get('/push/device', function (req, res) {
+  var recipient = {
+    deviceId: 'YOUR_DEVICE_ID'
+  };
+  var notification = {
+    notification: {
+      title: 'Hello from Ably!'
+    }
+  };
+  realtime.push.publish(recipient, notification, function(err) {
+    if (err) {
+      console.log('Unable to publish push notification; err = ' + err.message);
+      return;
+    }
+    console.log('Push notification published');
+    res.send("Push Sent");
+  });
+})
+
+app.get('/push/client', function (req, res) {
+  var recipient = {
+    clientId: 'ios:push'
+  };
+  var notification = {
+    notification: {
+      title: 'Hello from Ably!'
+    }
+  };
+  realtime.push.publish(recipient, notification, function(err) {
+    if (err) {
+      console.log('Unable to publish push notification; err = ' + err.message);
+      return;
+    }
+    console.log('Push notification published');
+    res.send("Push Sent");
+  });
+})
+
+app.get('/push/channel', function (req, res) {
+  var extras = {
+    push: {
+      notification: {
+        title: 'Hello from Ably!',
+        body: 'Example push notification from Ably.'
+      },
+      data: {
+        foo: 'bar',
+        baz: 'qux'
+      }
+    }
+  };
+  var channel = realtime.channels.get('push');
+  channel.publish({ name: 'example', data: 'data', extras: extras }, function(err) {
+    if (err) {
+      console.log('Unable to publish message with push notification; err = ' + err.message);
+      return;
+    }
+    console.log('Message with push notification published');
+    res.send("Push Sent");
+  });
+})
