50f146a0c6f8fa10a0b00abc6391454c2119e938
Christopher Batin <chris@batin.me>
2019-01-29 18:50:35 +0000

    Subscribe device

--- testappserver/index.js
+++ testappserver/index.js
@@ -16,3 +16,48 @@ app.get('/auth', function (req, res) {
     }
   });
 });
+
+app.get('/subscribe', (req, res) => {
+  console.log('Registering device')
+  //will need to inspect the request to see if this works
+  var deviceId = req.query.deviceId
+  console.log('Device Id ' + deviceId)
+  var deviceToken = req.query.deviceToken
+  var recipientDetails = {
+            transportType: 'apns',
+            //replace with actual device token
+            deviceToken: deviceToken
+  }
+  var device = {
+    id: deviceId,
+    formFactor: 'phone',
+    metadata: 'PST',
+          platform: 'ios',
+          push: {
+              recipient: recipientDetails
+          }
+  }
+  realtime.push.admin.deviceRegistrations.save(device, (err, device) => {
+      if(err){
+          console.log('Error: ' + err)
+          res.status(500).send('Error registering: ' + JSON.stringify(err));
+      } else{
+          console.log('Device registered:' + device.id)
+          subscribeDevice(device)
+      }
+      res.send('Complete')
+  })
+})
+function subscribeDevice (device){
+  var channelSub = {
+      channel: 'push',
+      deviceId: device.id
+  }
+  realtime.push.admin.channelSubscriptions.save(channelSub, (err, channelSub) => {
+      if(err){
+          console.log('Error: ' + err)
+      } else{
+          console.log('Device subscribed to push channel with deviceId' + device.id)
+      }
+  })
+}
