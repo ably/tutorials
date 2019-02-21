0f6daa8180648e505f438b258c235dff9613148d
Amit S <amit@smargav.com>
2019-02-21 11:46:01 +0530

    Inital app setup and dependencies

---
+++ Android/PushTutorial/app/src/main/java/io/ably/example/androidpushexample/MainActivity.java
@@ -0,0 +1,17 @@
+package io.ably.example.androidpushexample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Amit S.
+ *
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
