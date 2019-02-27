d44db117ab56e071d5fc858694253fadde98640d
Christopher Batin <chris@batin.me>
2019-02-27 11:47:53 +0000

    Node server registration

--- testappserver/index.js
+++ testappserver/index.js
@@ -18,3 +18,51 @@ app.get('/auth', function (req, res) {
     }
   });
 });
+
+app.get('/subscribe', (req, res) => {
+  console.log('Registering device')
+  //will need to inspect the request to see if this works
+  var deviceId = req.query.deviceId
+  console.log('Device Id ' + deviceId)
+  var deviceToken = req.query.deviceToken
+  var recipientDetails = {
+            transportType: 'apns',
+            //replace with actual device token
+            deviceToken: deviceToken
+  }
+    var myDevice = {
+      id: deviceId,
+      formFactor: 'phone',
+      metadata: 'PST',
+      platform: 'ios',
+      push: {
+          recipient: recipientDetails,
+          // state: 'Active'
+      }
+  }
+  realtime.push.admin.deviceRegistrations.save(myDevice, (err, device) => {
+      if(err){
+          console.log('Error: ' + err.message)
+          res.status(500).send('Error registering: ' + JSON.stringify(err));
+      } else{
+          console.log('Device registered:' + device.id)
+          subscribeDevice(device)
+          res.setHeader('Content-Type', 'application/json');
+	        res.send(device);
+      }
+  })
+})
+
+function subscribeDevice (device){
+  var channelSub = {
+      channel: 'push',
+      deviceId: device.id
+  }
+  realtime.push.admin.channelSubscriptions.save(channelSub, (err, channelSub) => {
+      if(err){
+          console.log('Error: ' + err)
+      } else{
+          console.log('Device subscribed to push channel with deviceId' + device.id)
+      }
+  })
+}
