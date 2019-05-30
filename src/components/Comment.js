992b55ca4f01f20fe523906755e7b6f6a67e129e
tomC <tc13270@my.bristol.ac.uk>
2019-05-30 14:22:33 +0100

    Step 10 – Create Comment Component

---
+++ src/components/Comment.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Comment extends Component {
+  render() {
+    return (
+      <article className="media">
+        <figure className="media-left">
+          <p className="image is-64x64">
+            <img src="https://bulma.io/images/placeholders/128x128.png" alt="Avatar" />
+          </p>
+        </figure>
+        <div className="media-content">
+          <div className="content">
+            <p>
+              <strong>{this.props.comment.name}</strong>
+              <br />
+              {this.props.comment.comment}
+            </p>
+          </div>
+        </div>
+      </article>
+    );
+  }
+}
+
+export default Comment;
