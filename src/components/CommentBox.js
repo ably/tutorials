8dad11a88566aef3b641b6bb840be2ae540715dc
tomC <tc13270@my.bristol.ac.uk>
2019-05-30 14:21:53 +0100

    Step 9 - Adding and Publishing Comment

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
- 
+
 class CommentBox extends Component {
   constructor(props) {
     super(props);
+    this.addComment = this.addComment.bind(this);
   }
- 
+
+  addComment(e) {
+    // Prevent the default behaviour of form submit
+    e.preventDefault();
+
+    // Get the value of the comment box
+    // and make sure it not some empty strings
+    const comment = e.target.elements.comment.value.trim();
+    const name = e.target.elements.name.value.trim();
+
+    // Make sure name and comment boxes are filled
+    if (name && comment) {
+      const commentObject = { name, comment };
+
+      // Publish comment
+      /*global Ably*/
+      const channel = Ably.channels.get('comments');
+      channel.publish('add_comment', commentObject, err => {
+        if (err) {
+          console.log('Unable to publish message; err = ' + err.message);
+        }
+      });
+
+      // Clear input fields
+      e.target.elements.comment.value = '';
+      e.target.elements.name.value = '';
+    }
+  }
+
   render() {
     return (
       <div>
@@ -30,5 +59,5 @@ class CommentBox extends Component {
     );
   }
 }
- 
+
 export default CommentBox;
