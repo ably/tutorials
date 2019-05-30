1e5a9903a066dd4fd247aaef7ce1451489d1cdb7
tomC <tc13270@my.bristol.ac.uk>
2019-05-30 14:15:42 +0100

    Step 7 – Install Ably

---
+++ src/ably.js
@@ -0,0 +1,4 @@
+import { Realtime } from 'ably/browser/static/ably-commonjs.js';
+ 
+alert("You need to add your API key"); /* REMOVE THIS */
+window.Ably = new Realtime('INSERT-YOUR-API-KEY-HERE'); // ADD YOUR API KEY HERE 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+require('./ably');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
