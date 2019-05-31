a4164d581d9dc849021228611523a11df0d2cb89
tomC <tc13270@my.bristol.ac.uk>
2019-05-31 09:32:36 +0100

    Step 15 – Displaying Comments

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,27 @@ class App extends Component {
     }
   }
 
+  componentDidMount() {
+    /* global Ably */
+    const channel = Ably.channels.get('comments');
+   
+    channel.attach();
+      channel.once('attached', () => {
+        channel.history((err, page) => {
+          /* create a new array with comments */
+          const comments = Array.from(page.items, item => item.data);
+
+          this.setState({ comments });
+
+          /* subscribe to new comments */
+          channel.subscribe((msg, err) => {
+            const commentObject = msg['data'];
+            this.handleAddComment(commentObject);
+          });
+        });
+      });
+  }
+
   handleAddComment(comment) {
     this.setState(prevState => {
       return {
