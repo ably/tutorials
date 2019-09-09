4a6a1759171df4e3916c7a58c4c9590cb344d7ed
Vivek <vivz.thats.me@gmail.com>
2019-09-09 11:53:43 -0400

    Step 3 - Publish encrypted messages

--- constants.js
+++ constants.js
@@ -3,6 +3,11 @@ if (API_KEY.indexOf('INSERT') === 0) {
     throw('Cannot run without an API key. Add your key to this file.');
 }
 
+/* Note: A sample cipher key is provided here for simplicity. Alternatively, one could use
+the Ably.Realtime.Crypto.generateRandomKey() utility method to generate a valid key */
+const SECRET = "AAECAwQFBgcICQoLDA0ODw==";
+
 module.exports = {
-    API_KEY: API_KEY
+    API_KEY: API_KEY,
+    SECRET: SECRET
 };
--- publisher.js
+++ publisher.js
@@ -3,3 +3,10 @@ const constants = require('./constants');
 
 /* Instance the Ably library */
 const realtime = new Ably.Realtime({key: constants.API_KEY});
+const channelOpts = {cipher: {key: constants.SECRET}};
+const pubChannel = realtime.channels.get('encrypted:messages', channelOpts);
+
+console.log('Publishing encrypted message');
+
+/* Publish encrypted messages */
+pubChannel.publish('unencrypted', 'an encrypted message');
