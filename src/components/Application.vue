30a8eac70749b96264bc83350d27772c0ba256b1
ogwurujohnson <ogwurujohnson@gmail.com>
2019-11-20 15:12:33 +0100

    implement map view

--- src/components/Application.vue
+++ src/components/Application.vue
@@ -1,5 +1,29 @@
+<template>
+  <div id="app">
+    <GmapMap
+      :center="{lat: initialPosition.lat, lng:initialPosition.lng}"
+      :zoom="10"
+      map-type-id="terrain"
+      style="width: 100%; height: 90%"
+    >
+      <!-- check if icon link in makers payload then display -->
+      <GmapMarker
+        :key="index"
+        v-for="(m, index) in markers"
         :position="m.position"
         :clickable="true"
         :draggable="false"
         @click="center=m.position"
         :icon="m.icon"
+        :title="m.userName"
+      />
+    </GmapMap>
+
+
+<script>
+import axios from "axios";
+import * as Ably from "ably";
+import Loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/vue-loading.css";
+
+      markers: [],
