69a0433a6fe3cf8df6285578af7b4245087e419a
ogwurujohnson <ogwurujohnson@gmail.com>
2019-11-20 15:23:16 +0100

    create homepage to serve as onboarding view

---
+++ src/components/Home.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="home">
+    <img src="../assets/cloud.svg" />
+    <div class="write-up">
+      <h4>Dear Friend,</h4>
+      <p>Welcome to Friend Finder the App</p>
+      <h3>Getting Started</h3>
+      <ul>
+        <li>Click on the big green button to start</li>
+        <li>Enter your name and click Enter</li>
+        <li>
+          Enter the name of your friends channel, or enter the name of a channel
+          you want to create for yoiu and your friends, then hit Enter
+        </li>
+        <li>Share your channel name with your friends to join</li>
+        <li>you can zoom in and out to see your friends fromt the map</li>
+        <li>
+          You would have detailed information about current friends in your
+          channel
+        </li>
+      </ul>
+      <a href="#/app" class="button">Get Started</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.home {
+  background: #f1f6fd;
+  height: 100vh;
+}
+
+img {
+  width: 40%;
+}
+
+.write-up {
+  margin-left: 3.0rem;
+}
+
+h4, h5 {
+  font-size: 25px;
+  font-weight: normal;
+}
+
+.button {
+  padding: 1.0rem;
+  border: 1px solid teal;
+  background: teal;
+  color: white;
+  font-size: 14px;
+  font-weight: bold;
+  text-decoration: none;
+  margin-top: 2.0rem;
+}
+</style>
