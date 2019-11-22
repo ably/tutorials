23a76718805821973663abda1b7db5b45f16ff54
codeekage <agiriabrahamjunior@gmail.com>
2019-11-22 10:20:12 +0100

    first commit

---
+++ hooks/ably.hooks.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react'
+import { ably } from '../env'
+
+//set CHANNEL_SCOPE  channel
+const CHANNEL_SCOPE = '<HUB_CHANNEL_SCOPE>'
+export const useAblyChannel = (channel, dependencies) => {
+
+  const [onMessage, setOnMessage] = useState('Please wait..')
+
+  const [isLoading, setLoading] = useState(true)
+  //fetch channel data
+  const [channelData, setChannelData] = useState(null)
+
+  useEffect(() => {
+    console.log('Ran Use Effects', channel)
+    ably.connection.on(function(stateChange) {
+      console.log('New connection state is ' + stateChange.current)
+      setOnMessage(stateChange.current)
+      setLoading(true)
+    })
+
+    const useChannel = ably.channels.get(`${CHANNEL_SCOPE}:${channel}`)
+    useChannel.subscribe(message => {
+      if (message.data.length > 0) {
+        setOnMessage('Loading Data...')
+        setLoading(false)
+        setChannelData(message.data)
+      }
+    })
+  }, dependencies)
+
+  return [isLoading, onMessage, channelData]
+}
