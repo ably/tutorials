0450fd864b124d556d303e3bf3a435b40e02b6b9
codeekage <agiriabrahamjunior@gmail.com>
2019-11-22 10:04:21 +0100

    first commit

---
+++ constants/Colors.js
@@ -0,0 +1,19 @@
+const tintColor = '#000';
+
+export default {
+  tintColor,
+  tabIconDefault: '#e6e3e3',
+  tabIconSelected: tintColor,
+  tabBar: '#000',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+  takeOffColor : "#7cbf80",
+  landingColor : "#c27e7e"
+};
+
+//#c27e7e
+//#7cbf80
