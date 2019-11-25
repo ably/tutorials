79cea8f46c2c0a038d68f449904cad6d0754aea4
ogwurujohnson <ogwurujohnson@gmail.com>
2019-11-25 08:06:52 +0100

    setup map view with default pointers

---
+++ src/components/Application.vue
@@ -0,0 +1,18 @@
+    <GmapMap
+      :center="{ lat: initialPosition.lat, lng: initialPosition.lng }"
+      :zoom="10"
+      map-type-id="terrain"
+      style="width: 100%; height: 90%"
+    >
+      <!-- check if icon link in makers payload then display -->
+      <GmapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="false"
+        @click="center = m.position"
+        :icon="m.icon"
+        :title="m.userName"
+      />
+    </GmapMap>
