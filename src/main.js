08cc1acb3692bf3f995bc24267313763c7e48d94
ogwurujohnson <ogwurujohnson@gmail.com>
2019-11-25 08:03:05 +0100

    initialise the vuegooglemaps package supplying the api keys from google

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
 import * as VueGoogleMaps from 'vue2-google-maps'
+ 
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: '<your key>',
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+    
+  },
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
