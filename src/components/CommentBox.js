4689eef90b22bdfafedc1b60d18dbdeae128e7d7
Marcel Koc <koc.marcel@gmail.com>
2020-10-22 11:45:24 +0200

    Step 9 – Adding and Publishing Comment

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
+import Ably from './Ably';
 
 class CommentBox extends Component {
   constructor(props) {
     super(props);
+    this.addComment = this.addComment.bind(this);
+  }
+
+  addComment(e) {
+    // Prevent the default behaviour of form submit
+    e.preventDefault();
+
+    // Get the value of the comment box
+    // and make sure it not some empty strings
+    const comment = e.target.elements.comment.value.trim();
+    const name = e.target.elements.name.value.trim();
+    // Get the current time.
+    const timestamp = Date.now();
+
+    // Make sure name and comment boxes are filled
+    if (name && comment) {
+      const commentObject = { name, comment, timestamp };
+
+      // Publish comment
+      const channel = Ably.channels.get('comments');
+      channel.publish('add_comment', commentObject, err => {
+        if (err) {
+          console.log('Unable to publish message; err = ' + err.message);
+        }
+      });
+
+      // Clear input fields
+      e.target.elements.comment.value = '';
+    }
   }
 
   render() {
