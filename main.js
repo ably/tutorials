797d4faab689a9c077fff98e1549c65d57dbda07
Tony Bedford <tony.bedford@live.co.uk>
2021-02-19 11:33:22 +0000

    Step 4 - Instantiating the Ably realtime library

--- main.js
+++ main.js
@@ -1,106 +1,38 @@
-let apiKey = 'YOUR_API_KEY';
-let ably = new Ably.Realtime({
-  key: apiKey,
-});
-
-let regularChannelName = "channel-" + Math.random().toString(36).substr(2, 16);
-let channelOpts = { params: { occupancy: "metrics" } };
-let channel = ably.channels.get(regularChannelName, channelOpts);
-let resultArea = document.getElementById("result");
-resultArea.scrollTop = resultArea.scrollHeight;
-
-channel.subscribe("[meta]occupancy", (msg) => {
-  let msgJSONobj = JSON.parse(JSON.stringify(msg));
-  console.log("MSG: " + msg);
-  //extract occupancy data from the message returned in the callback
-  let occupancyMetrics = msgJSONobj.data.metrics;
-  if (occupancyMetrics && msgJSONobj.name.includes("[meta]")) {
-    resultArea.value +=
-      "\n\n[METADATA - " +
-      new Date().toLocaleTimeString() +
-      ' ]: Occupancy on channel "' +
-      regularChannelName +
-      '" has been updated. New data is as follows:\n';
-    resultArea.value += "Connections: " + occupancyMetrics.connections + " \n";
-    resultArea.value += "Publishers: " + occupancyMetrics.publishers + " \n";
-    resultArea.value += "Subscribers: " + occupancyMetrics.subscribers + " \n";
-    resultArea.value +=
-      "Presence Connections: " + occupancyMetrics.presenceConnections + " \n";
-    resultArea.value +=
-      "Presence Members: " + occupancyMetrics.presenceMembers + " \n";
-    resultArea.value +=
-      "Presence Subscribers: " + occupancyMetrics.presenceSubscribers + " \n";
-    resultArea.scrollTop = resultArea.scrollHeight;
-  }
-});
-
-function addPublisherInstance() {
-  console.log("API KEY: " + apiKey);
-  resultArea.value +=
-    "\n[LOCAL LOG - " +
-    new Date().toLocaleTimeString() +
-    " ]: Adding new publisher instance\n";
-  let ably = new Ably.Realtime({
+const apiKey = '<YOUR_ABLY_KEY>'
+const ably = new Ably.Realtime({
     key: apiKey,
-  });
-  let regularChannel = ably.channels.get(regularChannelName);
-  console.log("adding publisher instance");
-  regularChannel.publish("test-data", {
-    data: "Dummy Data",
-    time: Date.now(),
-  });
+})
+
+const regularChannelName = 'channel-' + Math.random().toString(36).substr(2, 16)
+const channelOpts = { params: { occupancy: 'metrics' } }
+const channel = ably.channels.get(regularChannelName, channelOpts)
+const resultArea = document.getElementById('result')
+resultArea.scrollTop = resultArea.scrollHeight
+
+function localLog(msg) {
+    const logDate = new Date().toLocaleTimeString()
+    const template = `\n\n[LOCAL LOG - ${logDate}] - ${msg}\n`
+    resultArea.value += template
+    console.log(msg)
 }
 
-function addSubscriberInstance() {
-  resultArea.value +=
-    "\n[LOCAL LOG - " +
-    new Date().toLocaleTimeString() +
-    " ]: Adding new subscriber instance\n";
-  let ably = new Ably.Realtime({
-    key: apiKey,
-  });
-  let regularChannel = ably.channels.get(regularChannelName);
-  console.log("adding subscriber instance");
-  regularChannel.subscribe("test-data", (data) => {
-    //do whatever
-    console.log("Subscription working");
-  });
+function logData(channelName, metrics) {
+    const logDate = new Date().toLocaleTimeString()
+    const prompt = `\n\n[METADATA - ${logDate}] - Occupancy on channel ${channelName} has been updated: \n`
+    const template = `Connections: ${metrics.connections}
+Publishers: ${metrics.publishers}
+Subscribers: ${metrics.subscribers}
+Presence Connections: ${metrics.presenceConnections}
+Presence Members: ${metrics.presenceMembers}
+Presence Subscribers: ${metrics.presenceSubscribers}`
+
+    return prompt + template
 }
 
-function addPublisherInstanceWithPresence() {
-  resultArea.value +=
-    "\n[LOCAL LOG - " +
-    new Date().toLocaleTimeString() +
-    " ]: Adding new publisher instance\n";
-  let myId = "clientId-" + Math.random().toString(36).substr(2, 16);
-  let ably = new Ably.Realtime({
-    key: apiKey,
-    clientId: myId,
-  });
-  let regularChannel = ably.channels.get(regularChannelName);
-  console.log("adding publisher instance");
-  regularChannel.publish("test-data", {
-    data: "Dummy Data",
-    time: Date.now(),
-  });
-  regularChannel.presence.enter();
-}
-
-function addSubscriberInstanceWithPresence() {
-  resultArea.value +=
-    "\n[LOCAL LOG - " +
-    new Date().toLocaleTimeString() +
-    " ]: Adding new subscriber instance\n";
-  let myId = "clientId-" + Math.random().toString(36).substr(2, 16);
-  let ably = new Ably.Realtime({
-    key: apiKey,
-    clientId: myId,
-  });
-  let regularChannel = ably.channels.get(regularChannelName);
-  console.log("adding subscriber instance");
-  regularChannel.subscribe("test-data", (data) => {
-    //do whatever
-    console.log("Subscription working");
-  });
-  regularChannel.presence.enter();
-}
+channel.subscribe('[meta]occupancy', (msg) => {
+    const occupancyMetrics = msg.data.metrics
+    if (occupancyMetrics && msg.name.includes('[meta]')) {
+        resultArea.value += logData(regularChannelName, occupancyMetrics)
+        resultArea.scrollTop = resultArea.scrollHeight
+    }
+})
