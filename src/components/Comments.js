c5b38efa3d304f83be2f89a97b2217a45bb5f82a
marklewin <mark.lewin@ably.com>
2021-07-01 11:35:02 +0100

    Step 11 - Create Comments Component

---
+++ src/components/Comments.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+import Comment from "./Comment"
+
+class Comments extends Component {
+  render() {
+    return (
+      <section className="section">
+        {this.props.comments.map((comment, index) => {
+          return <Comment key={comment.timestamp} comment={comment} />
+        })}
+      </section>
+    )
+  }
+}
+export default Comments
