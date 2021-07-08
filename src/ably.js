993362e5bd0f4a11b456c3d2ddf34b2b4ddc534c
marklewin <mark.lewin@ably.com>
2021-07-08 14:33:05 +0100

    Start over

--- src/ably.js
+++
@@ -1,4 +0,0 @@
-import { Realtime } from 'ably/browser/static/ably-commonjs.js';
- 
-alert("You need to add your API key"); /* REMOVE THIS */
-window.Ably = new Realtime('INSERT-YOUR-API-KEY-HERE'); // ADD YOUR API KEY HERE 
