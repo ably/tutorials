522610700c3b7b8f847f322f96e23c7d0a271fd6
sacOO7 <sachinshinde7676@gmail.com>
2022-05-18 02:14:06 +0530

    Updated pusher-client, added subscription success/error callbacks

--- pusher-client.js
+++ pusher-client.js
@@ -23,12 +23,36 @@ pusher.bind_global((eventName, data)=> {
     console.log("eventName-" + eventName + " data-" + JSON.stringify(data));
 });
 
-const channel = pusher.subscribe('some_channel');
-channel.bind_global((eventName, data)=> {
-    console.log("channel :: eventName-" + eventName + " data-" + JSON.stringify(data));
+const public_channel = pusher.subscribe('public-channel');
+public_channel.bind("pusher:subscription_succeeded", () => {
+    console.log('subscribed to public-channel')
+});
+public_channel.bind("pusher:subscription_error", (err) => {
+    console.error('public channel subscription error', err)
+});
+public_channel.bind_global((eventName, data)=> {
+    console.log("private channel :: eventName-" + eventName + " data-" + JSON.stringify(data));
 });
 
-const channel1 = pusher.subscribe('private-channel');
-channel1.bind_global((eventName, data)=> {
-    console.log("channel :: eventName-" + eventName + " data-" + JSON.stringify(data));
+
+const private_channel = pusher.subscribe('private-channel');
+private_channel.bind("pusher:subscription_succeeded", () => {
+    console.log('subscribed to private-channel')
 });
+private_channel.bind("pusher:subscription_error", (err) => {
+    console.error('private channel subscription error', err)
+});
+private_channel.bind_global((eventName, data)=> {
+    console.log("private channel :: eventName-" + eventName + " data-" + JSON.stringify(data));
+});
+
+// const presence_channel = pusher.subscribe('presence-channel');
+// presence_channel.bind("pusher:subscription_succeeded", () => {
+//     console.log('subscribed to presence-channel')
+// });
+// presence_channel.bind("pusher:subscription_error", (err) => {
+//     console.error('presence channel subscription error', err)
+// });
+// presence_channel.bind_global((eventName, data)=> {
+//     console.log("private channel :: eventName-" + eventName + " data-" + JSON.stringify(data));
+// });
