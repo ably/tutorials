ac01578995ed6b7cd8322457f6b6e4736cdb5b99
sacOO7 <sachinshinde7676@gmail.com>
2022-05-18 22:29:15 +0530

    Refactored server.js file, updated symbols

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const Pusher = require("pusher");
 
-const API_KEY = ''; // Replace this with your API key
-if (API_KEY.indexOf("appid") === 0) {
+const API_KEY = 'appid.keyid:keysecret'; // Replace this with your API key
+if (API_KEY.indexOf("appid") === 0 || API_KEY == '') {
     throw("Cannot run without an API key. Add your key to example.js");
 }
 const APP_ID = API_KEY.split('.')[0],
@@ -11,7 +11,7 @@ KEY_PARTS = API_KEY.split(':'),
 KEY_NAME = KEY_PARTS[0],
 KEY_SECRET = KEY_PARTS[1];
 
-const pusher = new Pusher({
+const pusherRest = new Pusher({
     appId     : APP_ID,
     key       : KEY_NAME,
     secret    : KEY_SECRET,
@@ -32,19 +32,19 @@ app.get("/ping", (_req, res) => {
 
 // broadcast message to public channel
 app.get("/public", (_req, res) => {
-  pusher.trigger('public-channel', 'chat', { 'bot': 'Hi everyone' });
+  pusherRest.trigger('public-channel', 'chat', { 'bot': 'Hi everyone' });
   res.sendStatus(200);
 });
 
 // broadcast message to private channel
 app.get("/private", (_req, res) => {
-  pusher.trigger('private-channel', 'chat', { 'sac': 'Hi there' });
+  pusherRest.trigger('private-channel', 'chat', { 'sac': 'Hi there' });
   res.sendStatus(200);
 });
 
 // broadcast message to presence channel
 app.get("/presence", (_req, res) => {
-  pusher.trigger('presence-channel', 'chat', { 'jonny': 'I joined as a temporary person' });
+  pusherRest.trigger('presence-channel', 'chat', { 'jonny': 'I joined as a temporary person' });
   res.sendStatus(200);
 });
 
@@ -52,13 +52,13 @@ app.post("/ably/auth", (req, res) => {
   const socketId = req.body.socket_id;
   const channel = req.body.channel_name;
   if (channel.startsWith("private-")) {
-     res.send(pusher.authenticate(socketId, channel)); //  returns signed token containing socketId and channelName using config. secret
+     res.send(pusherRest.authenticate(socketId, channel)); //  returns signed token containing socketId and channelName using config. secret
   } else { 
     const presenceData = {
         user_id: "unique_user_id",
         user_info: { name: "Mr sac", twitter_id: "@pusher" },
     };
-    res.send(pusher.authenticate(socketId, channel, presenceData)); // returns signed token containing socketId, channelName and userData using config. secret
+    res.send(pusherRest.authenticate(socketId, channel, presenceData)); // returns signed token containing socketId, channelName and userData using config. secret
   }
 });
 
