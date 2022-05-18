3eaef837d5e08a89e3eff60665bee8ded4139c8f
sacOO7 <sachinshinde7676@gmail.com>
2022-05-18 22:27:53 +0530

    Refactored example.js, updated messages and symbols

--- example.js
+++ example.js
@@ -1,3 +1,4 @@
+console.warn("Only compatible with unix/bash shell, git bash can be used in windows")
 const Ably = require("ably");
 const Pusher = require("pusher");
 
@@ -26,24 +27,25 @@ ablyChannel.subscribe(function(message) {
 
 /* PUSHER CLIENT LIB */
 /* Instance the Pusher node library */
-const pusher = new Pusher({
+const pusherRest = new Pusher({
   appId     : APP_ID,
   key       : KEY_NAME,
   secret    : KEY_SECRET,
   host      : 'rest-pusher.ably.io',
-  encrypted : true,
-  uuid: '1234'
+  encrypted : true
 });
 
 
-console.log("Press p<enter> to publish a message with the Pusher nodejs client library, and see it be received by the Ably client library. Press q<enter> to quit.");
+console.log("Press p<enter> to publish a message with the Pusher nodejs library, and see it be received by the Ably client library. Press q<enter> to quit.");
 process.stdin.resume();
 process.stdin.on('data', function (chunk) {
   switch(chunk.toString()) {
     case "p\n":
-      pusher.trigger(pusherChannelName, 'eventName', { 'Some': 'JSON data' });
+    case "P\n":
+      pusherRest.trigger(pusherChannelName, 'eventName', { 'Some': 'JSON data' });
       break;
     case "q\n":
+    case "Q\n":
       ably.close();
       process.exit();
   }
